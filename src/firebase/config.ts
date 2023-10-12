109c1e759828e4194c8de84eda1da4b7c96b548a
Sayem <sayemahamed183@gmail.com>
2023-10-12 20:19:24 +0600

    adding recaptcha

---
+++ src/firebase/config.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCud-YTX1OSmAabpJ1EzjuDUF90ibqRjck",
+  authDomain: "rickandmortii.firebaseapp.com",
+  projectId: "rickandmortii",
+  storageBucket: "rickandmortii.appspot.com",
+  messagingSenderId: "641283146643",
+  appId: "1:641283146643:web:df715f243765fc66cf5e66",
+  measurementId: "G-X67KBFV88X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
